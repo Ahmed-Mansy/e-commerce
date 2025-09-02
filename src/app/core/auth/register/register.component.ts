9a959a9c41b6936e2150c6955899ff9858aacc40
Ahmed-Mansy <Ahmedalimansy1@gmail.com>
2025-09-02 03:52:44 +0300

    feat: implement registration form with validation and styling enhancements

--- src/app/core/auth/register/register.component.ts
+++ src/app/core/auth/register/register.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms"
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+registerForm = new FormGroup({
+  name: new FormControl(null , [Validators.required , Validators.minLength(3) , Validators.maxLength(20)]),
+  email: new FormControl(null , [Validators.required, Validators.email]),
+  password: new FormControl(null , [Validators.required , Validators.pattern(/^\w{6,}$/)]),
+  rePassword: new FormControl(null , [Validators.required , Validators.pattern(/^\w{6,}$/)]),
+  phone: new FormControl(null , [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]),
+})
+
+
+
+submitForm():void{
+  console.log(this.registerForm)
+}
 }
