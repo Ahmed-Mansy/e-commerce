36c8df6ea61137ad5b8c237fe912d953d3b19bcf
Ahmed-Mansy <Ahmedalimansy1@gmail.com>
2025-09-22 19:20:15 +0300

    refactor: remove unnecessary blank lines in server routes configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,8 +5,6 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'categories/subCategories/:id/:slug', renderMode: RenderMode.Server },
   { path: 'checkout/:id', renderMode: RenderMode.Server },
   { path: 'details/:slug/:id', renderMode: RenderMode.Server },
-
-
   {
     path: '**',
     renderMode: RenderMode.Prerender
